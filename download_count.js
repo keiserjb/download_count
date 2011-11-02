a9864e415e7979dc437b4f3ac5192dffd91a9134
worldfallz <worldfallz@hotmail.com>
2011-11-02 18:40:33 -0400

    No Issue by WorldFallz - fix js file.

--- download_count.js
+++ download_count.js
@@ -1,18 +1,18 @@
 (function ($) {
   Drupal.behaviors.download_count = {
     attach: function(context, settings) {
-      $('#edit-download-count-export-date-range-from-wrapper').hide();
-      $('#edit-download-count-export-date-range-to-wrapper').hide();
+      $('#download-count-export-form div.form-item-download-count-export-date-range-from').hide();
+      $('#download-count-export-form div.form-item-download-count-export-date-range-to').hide();
 
       $('input#edit-download-count-export-range-0').bind('click', function(){
-        $('#edit-download-count-export-date-range-from-wrapper').hide();
-        $('#edit-download-count-export-date-range-to-wrapper').hide();
+        $('#download-count-export-form div.form-item-download-count-export-date-range-from').hide();
+        $('#download-count-export-form div.form-item-download-count-export-date-range-to').hide();
         }
       )
 
       $('input#edit-download-count-export-range-1').bind('click', function(){
-        $('#edit-download-count-export-date-range-from-wrapper').show();
-        $('#edit-download-count-export-date-range-to-wrapper').show();
+        $('#download-count-export-form div.form-item-download-count-export-date-range-from').show();
+        $('#download-count-export-form div.form-item-download-count-export-date-range-to').show();
         }
       )
 }}})(jQuery);
